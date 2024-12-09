a11a3f2e65f95d7a501365dd8b683c478c9661d5
C0C01611 <160739032+C0C01611@users.noreply.github.com>
2024-12-09 09:27:05 -0500

    Delete pra.sh

--- pra.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-TIMESTAMP=$(date +"%F")
-BACKUP_DIR="/backup/$TIMESTAMP"
-mkdir -p "$BACKUP_DIR"
-# Sauvegarde de la base de données
-mysqldump -u dolibarruser -p'motdepasse' dolibarr > "$BACKUP_DIR/dolibarr.sql"
-# Sauvegarde des fichiers Dolibarr
-tar -czvf "$BACKUP_DIR/dolibarr_files.tar.gz" /var/www/html/dolibarr
-
