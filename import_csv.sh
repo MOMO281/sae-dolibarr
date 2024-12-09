166afa2bb285efbc225705e2503da31584571854
user <user@vbox.univ-rouen.fr>
2024-12-09 13:09:21 +0100

    oui

--- import_csv.sh
+++ import_csv.sh
@@ -2,23 +2,23 @@
 
 # Define the path to your test.csv and MariaDB credentials
 CSV_FILE="./test.csv"
-MYSQL_HOST="172.22.0.2"
+MYSQL_HOST="172.18.0.1"
 MYSQL_USER="${MYSQL_USER:-dolidbuser}"
 MYSQL_PASSWORD="${MYSQL_PASSWORD:-dolidbpass}"
 MYSQL_DB="${MYSQL_DATABASE:-dolidb}"
 
 # Example: Import the CSV file into a Dolibarr table (adjust the table name and columns as needed)
-TABLE_NAME="llx_socpeople"  # Replace with the appropriate Dolibarr table
+TABLE_NAME="llx_user"  # Replace with the appropriate Dolibarr table
 FIELDS_TERMINATED_BY=","
 ENCLOSED_BY='"'
 
-# Create a MySQL command to load the CSV into the specified table
-mysql --host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DB <<EOF
+# Commande pour importer un CSV
+mysql --host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD --local-infile=1 $MYSQL_DB <<EOF
 LOAD DATA LOCAL INFILE '$CSV_FILE'
 INTO TABLE $TABLE_NAME
 FIELDS TERMINATED BY '$FIELDS_TERMINATED_BY'
 ENCLOSED BY '$ENCLOSED_BY'
-IGNORE 1 LINES;  # Use this if the first line in your CSV is a header
+IGNORE 1 LINES;
 EOF
 
 # Optionally, you can verify if the data is imported by running a simple SELECT query.
